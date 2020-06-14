fe3e93f7d1067d0b89ae171c89b001a7d6eb418a
Lam <luutrinhlam@gmail.com>
2020-06-14 17:35:24 +0700

    Add sleep method

--- src/main/java/model/Animal.java
+++ src/main/java/model/Animal.java
@@ -5,4 +5,9 @@ public abstract class Animal {
     public void eat(){
         System.out.println("Animal eat");
     }
+
+    public void sleep(){
+        System.out.println("Animal eat");
+    }
+
 }
